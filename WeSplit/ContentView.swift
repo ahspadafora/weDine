d11fcd4c7663627a044c5e3214223aa774544ed7
Amber Spadafora <ahelenespadafora@gmail.com>
2020-10-06 15:35:05 -0400

    Add navigationBarTitle

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -31,7 +31,7 @@ struct ContentView: View {
                 Section {
                     Text("$\(checkAmount)")
                 }
-            }
+            }.navigationBarTitle("WeSplit")
         }
     }
 }
